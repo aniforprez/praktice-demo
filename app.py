238ce6e554869fde3063db34ce514dee39580cf0
Anirudh Sylendranath <ani.for.prez@gmail.com>
2018-07-28 14:56:59 +0530

    Make outgoing calls to numbers in DB if current date

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import csv
 import os
 from datetime import datetime
+
 from flask import Flask
+from twilio.rest import Client
+
 from models import Appointment, db
 app = Flask(__name__)
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://localhost/praktise-demo')
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+TWILIO_ACCOUNT_SID = 'ACa5dc926f56c7a65f81d63bcafe005dde'
+TWILIO_ACCOUNT_TOKEN = '10231467feb9da44c69ddc57b4359228'
+twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_ACCOUNT_TOKEN)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 with app.app_context():
     db.init_app(app)
     db.create_all()
@@ -27,8 +35,30 @@ def import_csv():
         except Exception as e:
             db.session.rollback()
             print(e)
+
+def make_twilio_call(phone_number, appointment_id):
+    call = twilio_client.calls.create(
+                        url='https://62e97129.ngrok.io/call_answered/' + str(appointment_id),
+                        to=phone_number,
+                        from_='+15594713206'
+                    )
+    print(call.sid)
+
+def make_appointment_calls():
+    appointments = Appointment.query.all()
+    for appointment in appointments:
+        if appointment.appointment_datetime.date() == datetime.today().date():
+            appointment_id = appointment.id
+            phone_number = appointment.phone_number
+            provider = appointment.provider
+
+            if provider == 'twilio':
+                make_twilio_call(phone_number, appointment_id)
+
 with app.app_context():
     import_csv()
+    make_appointment_calls()
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
